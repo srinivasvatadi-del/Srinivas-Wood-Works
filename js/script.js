d77f0305aa45358fa702165e6c218765dd8185ab
srinivasvatadi-del <varmaphani47@gmail.com>
2025-09-20 03:23:15 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Example: Simple alert for contact form
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! We will get back to you soon.");
+});
